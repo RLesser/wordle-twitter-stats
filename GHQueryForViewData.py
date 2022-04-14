6f3296e76d13dedb65303b356b458ad4568a87eb
Robert Lesser <bclesser@gmail.com>
2022-04-14 10:50:23 -0400

    ordered query to reduce diff size

--- GHQueryForViewData.py
+++ GHQueryForViewData.py
@@ -6,7 +6,11 @@ import pandas_gbq
 def get_wordle_rounds_count_data():
     project_id = os.environ.get("GCP_PROJECT")
     query = f"""
-      SELECT * FROM `{project_id}.main.wordle_rounds_count`
+      SELECT
+        * 
+      FROM `{project_id}.main.wordle_rounds_count`
+      ORDER BY
+        wordle_num, rounds
     """
 
     df = pandas_gbq.read_gbq(query, project_id=project_id)
